e01eccf1c288a4cc929b1ce978d49b6d0769d792
Balazs Somorjai <somorjai.balazs@danubisoft.hu>
2017-05-05 11:38:23 +0200

    NHF: Menu and loading data from file

--- SmartCocktail/SmartCocktail/SmartCocktail.cpp
+++ SmartCocktail/SmartCocktail/SmartCocktail.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <sstream>
+#include <stdlib.h>
 #include "SmartArray.hpp"
 #include "Cocktail.h"
 #include "Ingredient.h"
@@ -9,7 +11,9 @@
 
 using namespace std;
 
-int main()
+SmartArray<Cocktail *> *g_arrCocktail;
+
+void test()
 {
 	Cocktail cocktailTest01;
 	cocktailTest01.SetName( "Tequila Sunrise" );
@@ -41,6 +45,105 @@ int main()
 	Cocktail cocktailRead;
 	cocktailRead.ReadData( strstream );
 	cocktailRead.WriteData( cout );
+}
+
+void load_cocktails()
+{
+	ifstream f( "data.txt" );
+
+	if( f.fail() )
+	{
+		cerr << "Failed to open" << endl;
+		return;
+	}
+
+	while( !f.eof() )
+	{
+		Cocktail *pCocktail = new Cocktail();
+		pCocktail->ReadData( f );
+
+		g_arrCocktail->InsertLast( pCocktail );
+	}
+
+	f.close();
+}
+
+void menu_dialog()
+{
+	system( "cls" );
+	cout << "SmartCocktail" << endl << endl;
+
+	cout << "0. Exit" << endl;
+	cout << "1. List exixting cocktails" << endl;
+	cout << "2. Add cocktail" << endl;
+	cout << "3. Save" << endl;
+
+	cout << endl;
+}
+
+void list()
+{
+	system( "cls" );
+
+	for( int i = 0; i < g_arrCocktail->GetCount(); ++i )
+	{
+		cout << i + 1 << ". ";
+		cout << g_arrCocktail->GetPosition( i )->GetName() << endl;
+	}
+
+	cout << endl << endl;
+	cout << "0. To Exit" << endl;
+	cout << "#. To list raw data of cocktail" << endl;
+
+	int iChoice;
+	while( true )
+	{
+		cin >> iChoice;
+
+		if( iChoice == 0 )
+		{
+			return;
+		}
+		else
+		{
+			if( iChoice - 1 < g_arrCocktail->GetCount() )
+				g_arrCocktail->GetPosition( iChoice - 1 )->WriteData( cout );
+		}
+	}
+}
+
+void run()
+{
+	menu_dialog();
+
+	int iChoice;
+	while( true )
+	{
+		cin >> iChoice;
+
+		switch( iChoice )
+		{
+			case 0: return;
+			case 1: list(); break;
+
+			default:
+				break;
+		}
+
+		menu_dialog();
+	}
+}
+
+int main()
+{
+	SmartArray<Cocktail *> arrCocktail;
+	g_arrCocktail = &arrCocktail;
+
+	load_cocktails();
+	run();
+
+	for( int i = 0; i < arrCocktail.GetCount(); ++i )
+		delete arrCocktail.GetPosition( i );
 
 	return 0;
 }
